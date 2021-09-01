3f9eda9e08873bdc4938555daeb39883d0127478
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2021-09-01 15:51:04 +0600

    pos desing

--- app/Http/Controllers/Sell/PosController.php
+++ app/Http/Controllers/Sell/PosController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Sell;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class PosController extends Controller
 {
     public function index()
     {
-        return view('modules.sell.pos_sell_page');
+        $categories = Category::all();
+        $brands = Brand::all();
+        $customers = Contact::OnlyCustomer()->get();
+        return view('modules.sell.pos_sell_page', compact('customers','categories','brands'));
     }
 }
